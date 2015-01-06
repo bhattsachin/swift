89fabcadf3537e2f11d25a2618f714d3c57794ce
bhattsachin <bhattsachin@gmail.com>
2015-01-05 20:43:42 -0800

    variables and constructs

---
+++ Basic.playground/section-1.swift
@@ -0,0 +1,26 @@
+// Playground - noun: a place where people can play
+
+var array = [45, 89, 09, 7]
+array[2]
+
+var map = [ "one": 1, "two": 2, "three": 3]
+map["two"]
+
+let implicitDouble = 90.0
+let explicitDouble:Double = 90
+
+let apples = 3
+let appleSummary = "Total of \(apples) apples"
+
+var optionalString: String? = "hello"
+optionalString = nil
+optionalString = "NotOptional"
+
+var normalString: String = "regularhello"
+normalString = "test"
+
+//normalString = nil
+
+
+
+
